4777342ec35061e3a4b50e012bccc63618544525
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-06-16 16:06:14 -0300

    fixed useCallback and useEffect

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,5 @@
 import { useState, useMemo, useCallback, useEffect } from "react";
+import NewTask from "../newTask/NewTask";
 import Tasks from "../tasks/index";
 import Button from "../buttons";
 import Text from "../text/index";
@@ -11,40 +12,48 @@ const Home = () => {
   const [show, setShow] = useState(true);
   const [category, setCategory] = useState("toDo");
 
-  useEffect(() => {
-    api.get("api/tasks").then(({ data }) => {
-      setTasks(data);
-    });
-  }, []);
-
-  const handleAddTask = (e) => {
-    e.preventDefault();
-    try {
-      api.post("api/tasks", {
-        title,
-      });
-      api.get("api/tasks").then(({ data }) => {
-        setTasks(data);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const deleteTask = (e) => {
-    e.preventDefault();
-    console.log(e); //! fix this function
-
-    // try {
-    //   api.delete("api/tasks/");
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  };
-
   const handleTitle = (value) => {
     setTitle(value);
   };
+
+  const getTasks = useCallback(() => {
+    return api
+      .get("api/tasks")
+      .then(({ data }) => {
+        setTasks(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const createTask = useCallback(() => {
+    return api
+      .post("api/tasks", {
+        title,
+      })
+      .then(({ data }) => {
+        console.log(data);
+        getTasks();
+      })
+      .catch((err) => console.log(err));
+  }, [title, getTasks]);
+
+  const deleteTask = useCallback(
+    (e) => {
+      const { id } = e.target;
+      return api
+        .delete(`api/tasks/${id}`)
+        .then(({ data }) => {
+          console.log(data);
+          getTasks();
+        })
+        .catch((err) => console.log(err));
+    },
+    [getTasks]
+  );
+
+  useEffect(() => {
+    getTasks();
+  }, [createTask, deleteTask, getTasks]);
 
   return (
     <div className={styles.container}>
@@ -52,19 +61,13 @@ const Home = () => {
       <div className={styles.innerContainer}>
         <div className={styles.buttons}>
           <Button>Todo</Button>
-          <Button></Button>
-          <Button></Button>
-          <Button></Button>
         </div>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => handleTitle(e.target.value)}
+        <NewTask
+          title={title}
+          createTask={createTask}
+          handleTitle={handleTitle}
         />
-        <button onClick={handleAddTask}>aqui</button>
-        <div>
-          <Tasks tasks={tasks} onClick={deleteTask} />
-        </div>
+        <Tasks tasks={tasks} deleteTask={deleteTask} />
       </div>
     </div>
   );
--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,18 +1,23 @@
 import styles from "./tasks.module.css";
 import Button from "../buttons";
+import { memo } from "react";
 
-const Tasks = ({ tasks, onClick }) => {
+const Tasks = ({ tasks, deleteTask }) => {
+  console.log("tasks render");
+
   return (
     <>
       {tasks.map((task) => {
-        const _id = task._id;
+        const { _id } = task;
         return (
           <div key={_id} className={styles.container}>
             <div className={styles.title}>{task.title}</div>
             {task.comments ? (
               <div className={styles.comments}>{task.comments}</div>
             ) : null}
-            <Button onClick={() => onClick(_id)}>Deletar</Button>
+            <Button id={_id} onClick={deleteTask}>
+              Deletar
+            </Button>
           </div>
         );
       })}
@@ -20,4 +25,4 @@ const Tasks = ({ tasks, onClick }) => {
   );
 };
 
-export default Tasks;
+export default memo(Tasks);
