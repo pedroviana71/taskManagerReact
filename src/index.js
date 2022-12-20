81707dd5836238021b78476015626e4bd8a92014
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-12-20 18:22:34 -0300

    fixed routers and created login component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./components/home/index";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./app";
+import Login from "./components/login.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404</div>,
+  },
+  {
+    path: "teste",
+    element: <Login />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />}>
-            <Home />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
