1a6898073c6b7d8089fd222038aeeb5bec6a97c6
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-12-22 10:49:29 -0300

    added navigate and controlled input

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import { useRegisterMutation } from "../../app/api/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [register] = useRegisterMutation();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = await register({
-      email: "pedritoteste1@gmail.com",
-      password: "123456",
-      username: "pedro",
-    });
-    console.log(data);
+    try {
+      const data = await register({
+        email,
+        password,
+        username,
+      });
+      navigate("/login");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <section>
       <h1>Register</h1>
       <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
         <button type="submit" onClick={handleSubmit}>
-          Login
+          Registrar
         </button>
       </form>
     </section>
