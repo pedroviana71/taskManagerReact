e766e4d333ff62bc9250636f9ffd726e8e9f812e
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-12-22 21:16:05 -0300

    created register page

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { useRegisterMutation } from "../../app/api/authSlice";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import styles from "./index.module.scss";
 
 const Register = () => {
-  const [register] = useRegisterMutation();
   const navigate = useNavigate();
+  const [register] = useRegisterMutation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
@@ -25,37 +26,63 @@ const Register = () => {
   };
 
   return (
-    <section>
-      <h1>Register</h1>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Registrar
-        </button>
+    <section className={styles.registerSection}>
+      <h1 className={styles.title}>Registre-se!</h1>
+      <h4>
+        Insira seus dados para criar uma conta e começar a usar o nosso serviço.
+      </h4>
+      <form className={styles.form}>
+        <div className={styles.container}>
+          <label className={styles.label} htmlFor="email">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.container}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.container}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className={styles.registerButton}
+          >
+            Registrar
+          </button>
+        </div>
       </form>
+      <div className={styles.linkContainer}>
+        <h5>Já tem uma conta?</h5>
+        <Link to="/login" className={styles.link}>
+          Login.
+        </Link>
+      </div>
     </section>
   );
 };
