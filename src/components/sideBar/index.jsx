e76bca4a57d0062600d23c761c866d18e71b6a13
pedroviana71 <pedrohenrique.viana5@gmail.com>
2023-03-01 20:39:48 -0300

    fixes

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./index.module.scss";
 import { useGetUserQuery } from "../../app/api/tasksSlice";
-
+import { MdClose } from "react-icons/md";
+import useWindowDimensions from "../../utils/customHooks/useWindowDimensions";
 
 export const SideBar = ({ setShowMenu }) => {
   const navigate = useNavigate();
+  const { width } = useWindowDimensions();
   const { data } = useGetUserQuery();
 
   const handleLogout = () => {
@@ -13,10 +15,13 @@ export const SideBar = ({ setShowMenu }) => {
   };
 
   return (
-    <div className={styles.container}>
+    <div className={width > 900 ? styles.container : styles.mobileContainer}>
       <p>{data?.username}</p>
       <p>{data?.email}</p>
       <button onClick={handleLogout}>Log Out</button>
+      <button onClick={() => setShowMenu(false)}>
+        <MdClose />
+      </button>
     </div>
   );
 };
